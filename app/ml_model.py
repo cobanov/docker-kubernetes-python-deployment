cdf6f754581cda989a63d7d9b0cfed5332b0d2f4
Mert Cobanov <mertcobanov@gmail.com>
2021-12-10 16:34:07 +0300

    first commit

---
+++ app/ml_model.py
@@ -0,0 +1,18 @@
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+import joblib
+
+
+constant = np.random.randint(0, 5, 50)
+x = np.random.randint(0, 10, 50)
+y = constant + 3 * x
+
+df = pd.DataFrame({'x': x, 'y': y})
+
+model = LinearRegression()
+model.fit(x.reshape(-1, 1), y)
+
+joblib.dump(model, 'my_model')
+
+# j_model = joblib.load('./my_model')
